580d061fb2f73333a6121e7d31a7bd5d1d1435f4
Heather Leyssenaar <48036336+lysna87@users.noreply.github.com>
2019-11-15 11:28:45 +1030

    Add project files.

---
+++ HiveMaintenanceV2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HiveMaintenanceV2.MvcApplication" Language="C#" %>
---
+++ HiveMaintenanceV2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HiveMaintenanceV2.Startup))]
+namespace HiveMaintenanceV2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
